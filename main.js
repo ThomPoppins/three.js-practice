6a8555a06d643f33398cc88d8de165198d9c5a16
Thom Poppins <thompoppins@gmail.com>
2023-07-18 22:40:48 +0100

    create a cross shape from 2 bar objects and rotate them sychnronized so it will give the illusion of being 2 object instead of 2

--- main.js
+++ main.js
@@ -29,16 +29,22 @@ document.body.appendChild(renderer.domElement);
 //
 // Wikipedia: "Geometry is a branch of mathematics concerned with properties of space such as the distance,
 // shape, size, and relative position of figures."
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+/* const geometry = new THREE.BoxGeometry(1, 1, 1); */
+const geometry2 = new THREE.BoxGeometry(1, 5, 1);
+const geometry3 = new THREE.BoxGeometry(1, 1, 5);
 // Initilize the material configuration with MeshBasicMaterial and set the color to 0x00ff00
 // by passing a object with the "color" key. 0x00ff00 is a green color.
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 // Initialize the cube by use of the "Mesh" object. 
 //
 // "Mesh" takes a geometry and applies a material to it, creating a cube in the process.
-const cube = new THREE.Mesh(geometry, material);
+// const cube = new THREE.Mesh(geometry, material);
+const bar1 = new THREE.Mesh(geometry2, material);
+const bar2 = new THREE.Mesh(geometry3, material);
 // The cube will be added to the scene at position with coordinates: (0,0,0)
-scene.add(cube);
+// scene.add(cube);
+scene.add(bar1);
+scene.add(bar2);
 
 // Because the camera was also on position with coordinates (0,0,0),
 // we change it's position to a different location so we can see the cube. 
@@ -50,11 +56,15 @@ camera.position.z = 5;
 function animate() {
   requestAnimationFrame(animate);
 
-  // Add a rotation effect to the cube object.
-  cube.rotation.x += 0.07;
-  cube.rotation.y += 0.03;
+  // Add a rotation effect to the bar objects
+  bar1.rotation.x += 0.04;
+  bar1.rotation.y += 0.02;
+  bar2.rotation.x += 0.04;
+  bar2.rotation.y += 0.02;
 
   // render the animation with params "scene" and "camera"
   renderer.render(scene, camera);
 }
+
+// call function animate, which will loop on itselve after calling  it once.
 animate();
